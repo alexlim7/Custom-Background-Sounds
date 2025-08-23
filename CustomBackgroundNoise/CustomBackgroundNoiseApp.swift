3e308db2f95103f4e10f58bca17f8370e95be96e
alexlim7 <alexander_j_lim@brown.edu>
2025-08-23 13:38:21 -0400

    Initial Commit

---
+++ CustomBackgroundNoise/CustomBackgroundNoiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBackgroundNoiseApp.swift
+//  CustomBackgroundNoise
+//
+//  Created by Alex Lim on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomBackgroundNoiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
